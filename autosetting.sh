e32f42496b45aab75768e2962607e3b8cc409db0
Taihei Morikuni <taihei.morikuni@gmail.com>
2015-01-10 16:47:12 +0900

    NeoBundleのインストールを自動化

--- autosetting.sh
+++ autosetting.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+#vim用ディレクトリの作成
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 mkdir ~/.vim/.undo
 mkdir ~/.vim/.swap
 
+#dotファイルのシンボリックリンクを作成
 dotfiles=(
 .vimrc
 .vimrc.basic 
@@ -13,9 +15,10 @@ dotfiles=(
 .vimrc.bundle
 .vim
 )
-
 for dotfile in ${dotfiles[@]}
 do
 	ln -sf ~/dotfiles/$dotfile ~/$dotfile 
 done
 
+#NeoBundleをインストール
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
