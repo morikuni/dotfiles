34c79ac528310c6d52a8d1b38e889dd6e90bbff4
morikuni <taihei.with+git@gmail.com>
2018-07-14 20:13:58 +0900

    install gui app

--- install.sh
+++ install.sh
@@ -4,4 +4,38 @@ if [ "$(uname)" == "Darwin" ]; then
     brew install rmtrash
     brew install tmux
     brew install fzf
+
+    brew cask install bettertouchtool
+    brew cask install alfred
+    brew cask install deckset
+    brew cask install font-fira-code
+    brew cask install dropbox
+    brew cask install jetbrains-toolbox
+    brew cask install kindle
+    brew cask install slack
+    brew cask install sequel-pro
+    brew cask install atok
+
+	# Translate Tab
+	mas install 458887729
+	# Kaleidoscope
+	mas install 587512244
+	# Moom
+	mas install 419330170
+	# 1Password 7
+	mas install 1333542190
+	# Todoist
+	mas install 585829637
+	# Tweetbot
+	mas install 557168941
+	# Fantastical 2
+	mas install 975937182
+	# Soulver
+	mas install 413965349
+	# Yoink
+	mas install 457622435
+	# PopClip
+	mas install 445189367
 fi
+
+
