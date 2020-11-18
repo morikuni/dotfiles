f13e3c22956a017d8548cc3371b22ed5ef9cd564
morikuni <taihei.with+git@gmail.com>
2020-11-18 09:53:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,46 +16,43 @@ brew install make
 brew install yarn
 
 if [ "$(uname)" == "Darwin" ]; then
-  brew cask install bettertouchtool
-  brew cask install alfred
-  brew cask install deckset
-  brew cask install jetbrains-toolbox
-  brew cask install dropbox
-  brew cask install font-fira-code
-  brew cask install kindle
-  brew cask install slack
-  brew cask install sequel-pro
-  brew cask install atok
-  brew cask install fork
-  brew cask install iterm2
-  brew cask install dash
-  brew cask install ksdiff
-  brew cask install google-cloud-sdk
-  brew cask install docker
-  brew cask install paw
+    brew cask install bettertouchtool
+    brew cask install alfred
+    brew cask install jetbrains-toolbox
+    brew cask install dropbox
+    brew cask install font-fira-code
+    brew cask install kindle
+    brew cask install slack
+    brew cask install sequel-pro
+    brew cask install atok
+    brew cask install fork
+    brew cask install iterm2
+    brew cask install dash
+    brew cask install ksdiff
+    brew cask install google-cloud-sdk
+    brew cask install docker
+    brew cask install paw
 
-	# Translate Tab
-	mas install 458887729
-	# Kaleidoscope
-	mas install 587512244
-	# Moom
-	mas install 419330170
-	# 1Password 7
-	mas install 1333542190
-	# Todoist
-	mas install 585829637
-	# Tweetbot
-	mas install 557168941
-	# Fantastical 2
-	mas install 975937182
-	# Soulver
-	mas install 413965349
-	# Yoink
-	mas install 457622435
-	# PopClip
-	mas install 445189367
-  # Spark
-  mas install 1176895641
+    # Translate Tab
+    mas install 458887729
+    # Kaleidoscope
+    mas install 587512244
+    # Moom
+    mas install 419330170
+    # 1Password 7
+    mas install 1333542190
+    # Tweetbot
+    mas install 557168941
+    # Fantastical 2
+    mas install 975937182
+    # Soulver
+    mas install 413965349
+    # Yoink
+    mas install 457622435
+    # PopClip
+    mas install 445189367
+    # Spark
+    mas install 1176895641
 fi
 
 
