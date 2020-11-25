6ce6c9fca9ad39cc9ce76446744486895d2e1310
morikuni <taihei.with+git@gmail.com>
2020-11-25 16:49:38 +0900

    Support main branch

--- fish/functions/merged.fish
+++ fish/functions/merged.fish
@@ -1,7 +1,7 @@
 function merged -a deploy
     if [ "$deploy" = "deploy" ]
-        git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master | xargs -I % git branch -d %
+        git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master | grep -v main |  xargs -I % git branch -d %
     else
-        git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master
+        git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master | grep -v main
     end
 end
