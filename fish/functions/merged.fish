b76c53978955660b42cf5d330e0b82ced47d2e02
morikuni <taihei.with+git@gmail.com>
2021-11-15 14:10:28 +0900

    Modify option name of merged command

--- fish/functions/merged.fish
+++ fish/functions/merged.fish
@@ -1,5 +1,5 @@
-function merged -a deploy
-    if [ "$deploy" = "deploy" ]
+function merged -a cmd
+    if [ "$cmd" = "apply" ]
         git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master | grep -v main |  xargs -I % git branch -d %
     else
         git branch --merged | grep -v (git symbolic-ref --short HEAD) | grep -v master | grep -v main
