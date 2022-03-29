56a2a689565063196febac9eeba9f3dd01a672c5
morikuni <taihei.with+git@gmail.com>
2022-03-29 15:14:44 +0900

    Inherit PATH

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,7 @@ set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
 # PATH
 set -gx fish_user_paths /opt/homebrew/bin
-set -gx fish_user_paths /usr/local/bin
+set -gx fish_user_paths /usr/local/bin $fish_user_paths
 set -gx fish_user_paths ~/bin $fish_user_paths
 set -gx fish_user_paths $GOPATH/bin $fish_user_paths
 switch (uname)
