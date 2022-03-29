a9e8f4d89351507752d8b60179a48d747f333d5e
morikuni <taihei.with+git@gmail.com>
2022-03-29 14:58:10 +0900

    Add path to homebrew

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,7 @@ set -gx EDITOR vim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
 # PATH
+set -gx fish_user_paths /opt/homebrew/bin
 set -gx fish_user_paths /usr/local/bin
 set -gx fish_user_paths ~/bin $fish_user_paths
 set -gx fish_user_paths $GOPATH/bin $fish_user_paths
