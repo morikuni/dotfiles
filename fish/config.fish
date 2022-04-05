574c29c9b1dfed7c1b8c11365543a37d64001fd0
morikuni <taihei.with+git@gmail.com>
2022-04-05 11:40:57 +0900

    Refactor fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,24 @@
+# ENV
+set -gx GOPATH ~/go
+set -gx EDITOR vim
+set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+
+switch (uname)
+case Linux
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+case Darwin
+    eval (/opt/homebrew/bin/brew shellenv)
+	source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+end
+
+
+# PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH $GOPATH/bin $PATH
+
+# direnv
+eval (direnv hook fish)
+
 # 安全に
 alias cp "cp -i"
 alias mv "mv -i"
@@ -5,26 +26,3 @@ if which trash > /dev/null 2>&1
 	alias rm "trash -F"
 end
 
-# ENV
-set -gx GOPATH ~/go
-set -gx EDITOR vim
-set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
-
-# PATH
-set -gx fish_user_paths /opt/homebrew/bin
-set -gx fish_user_paths /usr/local/bin $fish_user_paths
-set -gx fish_user_paths ~/bin $fish_user_paths
-set -gx fish_user_paths $GOPATH/bin $fish_user_paths
-switch (uname)
-case Linux
-	set -gx fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
-end
-
-# gcloud
-switch (uname)
-case Darwin
-	source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-end
-
-# direnv
-eval (direnv hook fish)
