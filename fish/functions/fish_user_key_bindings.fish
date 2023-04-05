d69acc6ad62ed764f34e0f2be1a52d880912ca8a
morikuni <taihei.with+git@gmail.com>
2023-04-05 16:58:01 +0900

    Set prevd and nextd

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings
     bind \cd "printf '\033[01Guse `exit` to close session\n'"
+    bind \cp "prevd; commandline -f execute"
+    bind \cn "nextd; commandline -f execute"
 end
