0e3fea3f5fd8f799b18fb99ec67750dba35a4ff4
morikuni <swishy.studded_0n@icloud.com>
2023-12-11 15:34:56 +0900

    Update new-branch command

--- fish/functions/new-branch.fish
+++ fish/functions/new-branch.fish
@@ -2,11 +2,16 @@ function new-branch -a base -a name
     if test -z "$base" -o -z "$name"
         echo "git-newbranch [base] [name]"
         return 1
+    end
+
+    set -l stash_result (git stash save)
+    git switch $base
+    git pull
+    git switch -c $name
+
+    if string match -qr 'No local changes to save' -- $stash_result
+        echo "No changes stashed."
     else
-        git stash save
-        git switch $base
-        git pull
-        git switch -c $name
         git stash pop
     end
 end
